0a0d173319f561c2bbebd33333c46e57479dd26e
Nisarg <nisargs2001@gmail.com>
2022-02-11 22:41:08 +0530

    added search functionality

--- dictionary/webpages/views.py
+++ dictionary/webpages/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from PyDictionary import PyDictionary
 # Create your views here.
 def home(request):
     return render(request, 'webpages/home.html')
+
+def result(request):
+    word=request.GET['word']
+    dictionary=PyDictionary()
+    meaning=dictionary.meaning(word)
+    synonyms=dictionary.synonym(word)
+    antonyms=dictionary.antonym(word)
+    data={
+        'word':word,
+        'meaning':meaning,
+        'synonyms':synonyms,
+        'antonyms':antonyms,
+    }
+    return render(request, 'webpages/result.html',data)
