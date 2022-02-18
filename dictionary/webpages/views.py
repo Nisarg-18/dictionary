ef9c0479e7b75a1e26304c5e8ca5c3d61413f4fa
Nisarg <nisargs2001@gmail.com>
2022-02-18 17:51:23 +0530

    changes in UI

--- dictionary/webpages/views.py
+++ dictionary/webpages/views.py
@@ -8,12 +8,10 @@ def result(request):
     word=request.GET['word']
     dictionary=PyDictionary()
     meaning=dictionary.meaning(word)
-    synonyms=dictionary.synonym(word)
-    antonyms=dictionary.antonym(word)
+  
     data={
         'word':word,
         'meaning':meaning,
-        'synonyms':synonyms,
-        'antonyms':antonyms,
+  
     }
     return render(request, 'webpages/result.html',data)
